023b87d97118cf423a7d8df516fc459b6ac35460
Julia Pascual <161069022+juliaapascual@users.noreply.github.com>
2024-06-23 20:34:55 -0300

    Atualização da lista de funcionários

---
+++ front-html/scripts/addEmployee.js
@@ -0,0 +1,56 @@
+function handleSubmit(event) {
+  event.preventDefault();
+
+  // Captura dos dados do formulário
+  const formData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    position: document.getElementById("position").value,
+    salary: parseFloat(document.getElementById("salary").value),
+    transportAllowance: document.getElementById("transportAllowance").checked,
+    department_id: parseInt(document.getElementById("department").value),
+  };
+
+  console.log(formData);
+
+  // Requisição POST usando Axios
+  axios
+    .post("http://localhost:3001/employees", formData)
+    .then((response) => {
+      console.log("Dados enviados com sucesso:", response.data);
+    })
+    .catch((error) => {
+      console.error("Erro ao enviar dados:", error);
+    });
+}
+document
+  .getElementById("employeeForm")
+  .addEventListener("submit", handleSubmit);
+
+// Função para preencher o select com os departamentos
+function populateDepartmentsSelect() {
+  // Seleciona o elemento select
+  const departmentSelect = document.getElementById("department");
+
+  // Faz a requisição para obter os departamentos
+  axios
+    .get("http://localhost:3001/departments")
+    .then((response) => {
+      const departments = response.data;
+
+      // Limpa as opções existentes no select
+      departmentSelect.innerHTML = "";
+
+      departments.forEach((department) => {
+        const option = document.createElement("option");
+        option.value = department.id;
+        option.textContent = department.name;
+        departmentSelect.appendChild(option);
+      });
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar departamentos:", error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", populateDepartmentsSelect);
